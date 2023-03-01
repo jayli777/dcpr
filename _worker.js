aa54028f05067fcb9f3a67795844c8fbf315fada
jayli777 <121683300+jayli777@users.noreply.github.com>
2023-03-01 23:39:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="misxijit.eu-ggqrtyuigfhs.dopraxrocks.net";
+        url.hostname="fcenaq.eu-hhwtruiofdjd.dopraxrocks.net";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
