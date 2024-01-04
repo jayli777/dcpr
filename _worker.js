7b43effae73c241d93acc4b733f2ee0cc415d950
jayli777 <121683300+jayli777@users.noreply.github.com>
2024-01-05 00:02:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="ja-31m3.onrender.com";
+        url.hostname="dc2bf827-9b59-4e79-9d0f-c7ae7214c194-00-cz68bduz3efw.global.replit.dev";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
