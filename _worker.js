b8dc1d76c271952a8683cecd6d22df86e7cd35f3
jayli777 <121683300+jayli777@users.noreply.github.com>
2024-01-13 23:15:36 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="dc2bf827-9b59-4e79-9d0f-c7ae7214c194-00-cz68bduz3efw.global.replit.dev";
+        url.hostname="34fa031d-0607-4553-8fe6-b736315bdb79-00-1hacpp1eb4se5.janeway.replit.dev";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
